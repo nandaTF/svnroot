ad5535fe4fca7468efe33141ec40f0446d7d6148
dmurygin <dmurygin@4c0145ca-515c-4d1a-a6bc-f9bd594c3410>
2010-05-03 14:50:22 +0000

    bug 151 - Create A Self Assessment View, initial import

---
+++ sernet.verinice.samt.rcp/src/org/verinice/samt/rcp/SamtPerspective.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Daniel Murygin.
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Daniel Murygin <dm[at]sernet[dot]de> - initial API and implementation
+ ******************************************************************************/
+package org.verinice.samt.rcp;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+import sernet.gs.ui.rcp.main.bsi.views.FileView;
+import sernet.gs.ui.rcp.main.bsi.views.NoteView;
+import sernet.gs.ui.rcp.main.bsi.views.RelationView;
+import sernet.verinice.iso27k.rcp.CatalogView;
+import sernet.verinice.iso27k.rcp.ISMView;
+
+/**
+ * @author Daniel Murygin <dm@sernet.de>
+ *
+ */
+public class SamtPerspective implements IPerspectiveFactory {
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+     */
+    @Override
+    public void createInitialLayout(IPageLayout layout) {
+        String editorArea = layout.getEditorArea();
+        layout.setEditorAreaVisible(true);    
+        layout.addView(SamtView.ID,  IPageLayout.LEFT, 0.3f, editorArea);  
+        layout.getViewLayout(SamtView.ID).setCloseable(false);
+
+    }
+
+}
