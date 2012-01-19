8e8680bbc5b89a600f7a05e0aa337a014af11b55
dmurygin <dmurygin@4c0145ca-515c-4d1a-a6bc-f9bd594c3410>
2012-01-19 13:31:43 +0000

    - Version number in splash screen updated to 1.5.x

--- BRANCHES/1.4/sernet.gs.ui.rcp.main/etc/sql/roleback-to-98.sql
+++ BRANCHES/1.4/sernet.gs.ui.rcp.main/etc/sql/roleback-to-98.sql
@@ -3,6 +3,6 @@ ALTER TABLE CNATREEELEMENT DROP COLUMN SCOPE_ID;
 
 UPDATE CNATREEELEMENT set DBVERSION=0.98 WHERE OBJECT_TYPE='bsimodel';
 
-UPDATE properties SET propertyvalue='user-group'  WHERE propertyvalue='user-default-group' AND propertytype='configuration_rolle';
+UPDATE properties SET propertyvalue='my-user-group'  WHERE propertyvalue LIKE 'user-default-group' AND propertytype = 'configuration_rolle';
 
-UPDATE properties SET propertyvalue='admin-group'  WHERE propertyvalue='admin-default-group' AND propertytype='configuration_rolle';
+UPDATE properties SET propertyvalue='my-admin-group'  WHERE propertyvalue LIKE 'admin-default-group' AND propertytype = 'configuration_rolle';
